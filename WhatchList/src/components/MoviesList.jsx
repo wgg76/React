a67e567f46b0789dee14cd4b161a9ff757847f46
wgg76 <167985626+wgg76@users.noreply.github.com>
2025-03-11 17:56:27 -0300

    Update MoviesList.jsx

--- WhatchList/src/components/MoviesList.jsx
+++ WhatchList/src/components/MoviesList.jsx
@@ -4,7 +4,7 @@ import MovieCard from "./MovieCard";
 
 const MoviesList = ({ addToWatchlist }) => {
   return (
-    <div className="p-50 grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-15">
+    <div className="p-30 grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-15">
       {movies.map((movie) => (
         <MovieCard key={movie.id} movie={movie} onAdd={addToWatchlist} />
       ))}
